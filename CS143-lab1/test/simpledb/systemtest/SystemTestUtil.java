e18c8dc165c850643ac9e79efbe00de8e348e8d4
katsukihi <katsukihi@users.noreply.github.com>
2015-02-03 13:19:15 -0800

    lab2 initial

--- CS143-lab1/test/simpledb/systemtest/SystemTestUtil.java
+++ CS143-lab1/test/simpledb/systemtest/SystemTestUtil.java
@@ -125,7 +125,7 @@ public class SystemTestUtil {
             boolean isExpected = copy.remove(list);
             Debug.log("scanned tuple: %s (%s)", t, isExpected ? "expected" : "not expected");
             if (!isExpected) {
-            	Assert.fail("expected tuples does not contain: " + t);
+                Assert.fail("expected tuples does not contain: " + t);
             }
         }
         iterator.close();
